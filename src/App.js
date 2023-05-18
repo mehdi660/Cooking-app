616324d63f72205aa137f7e2d3f170871b5dcc1b
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-05-18 18:42:50 +0200

    ajout de la logique de l'input pour rechercher les recettes

--- src/App.js
+++ src/App.js
@@ -4,26 +4,40 @@ import Card from "./components/Card";
 
 const App = () => {
   const [mealsData, setMealsData] = useState([]);
+  const [searchValue, setSearchValue] = useState(""); // État pour stocker la valeur de recherche
+
   useEffect(() => {
     axios
       .get("https://www.themealdb.com/api/json/v1/1/search.php?s")
       .then((res) => setMealsData(res.data.meals));
   }, []);
 
+  // Gestionnaire d'événements pour la saisie de recherche
+  const handleSearch = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  // Filtrer les plats en fonction de la valeur de recherche et les trier alphabétiquement
+  const filteredMeals = mealsData
+    .filter((meal) =>
+      meal.strMeal.toLowerCase().includes(searchValue.toLowerCase())
+    )
+    .sort((a, b) => a.strMeal.localeCompare(b.strMeal));
+
   return (
     <div className="app-container">
       <h1>React Cooking App</h1>
       <input
-        id="shearch"
+        id="search"
         type="text"
         placeholder="Tapez le nom d'un aliment (en anglais)"
+        value={searchValue}
+        onChange={handleSearch}
       />
       <div className="meals-container">
-        {mealsData
-          .sort((a, b) => a.strMeal.localeCompare(b.strMeal))
-          .map((meal) => (
-            <Card key={meal.idMeal} meal={meal} />
-          ))}
+        {filteredMeals.map((meal) => (
+          <Card key={meal.idMeal} meal={meal} />
+        ))}
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,10 @@ const Card = ({ meal }) => {
       <em>Origine : {meal.strArea}</em>
       <img src={meal.strMealThumb} alt={"Présentation de " + meal.strMeal} />
       <p>{meal.strInstructions}</p>
+      <a id="lien-recette" href={meal.strYoutube}>
+        Vidéo de la recette
+      </a>
+      {/* <p> Vidéo de la recette : {meal.strYoutube}</p> */}
     </div>
   );
 };
